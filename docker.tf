28bfaeffd307ff802154ff629f00ec8ade541a90
harshatejaadduri <143252561+harshatejaadduri@users.noreply.github.com>
2025-07-24 12:27:13 +0530

    updated

--- docker.tf
+++ docker.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "docker" {
+resource "aws_instance" "k8" {
   ami           = local.ami_id
   instance_type = "t3.medium"
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
@@ -11,7 +11,7 @@ resource "aws_instance" "docker" {
   user_data = file("docker.sh")
   #iam_instance_profile = "TerraformAdmin"
   tags = {
-     Name = "${var.project}-${var.environment}-docker"
+     Name = "Kubernetes"
   }
 }
 
