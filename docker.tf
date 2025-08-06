d953ed8999e474c507b3a8b906f01b97dd957cec
harshatejaadduri <harshateja3@gmail.com>
2025-08-06 13:37:19 +0530

    updated

--- docker.tf
+++ docker.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "k8" {
   ami           = local.ami_id
-  instance_type = "t3.medium"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
 
   # need more for docker
